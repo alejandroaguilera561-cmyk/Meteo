31e5cae70762139c5d3a6137e86a21737076c7c9
alejandroaguilera561-cmyk <alejandroaguilera561@gmail.com>
2026-05-21 18:42:49 -0300

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,21 @@
 const CACHE_NAME = "pronostico-zap-v1";
 
-const archivos = [
+const urlsToCache = [
   "./",
   "./index.html",
-  "./style.css",
-  "./app.js",
   "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(archivos))
+      .then(cache => cache.addAll(urlsToCache))
   );
-
-});
-
-self.addEventListener("activate", event => {
-
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-
 });
 
 self.addEventListener("fetch", event => {
-
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-
-        return response || fetch(event.request);
-
-      })
+      .then(response => response || fetch(event.request))
   );
-
 });
